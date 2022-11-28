4cfcf5899438e93a4ca47fed738ca7b25a6f6d73
meetdpatel18 <meetdpatel18@gmail.com>
2022-11-28 11:26:50 +0530

    time calculation updated Q3

--- q3.cpp
+++ q3.cpp
@@ -1,161 +1,148 @@
-#include<sys/types.h>
-#include<sys/shm.h>
-#include<sys/ipc.h>
-#include<unistd.h>
-#include<time.h>
-#include<iostream>
-#include<pthread.h>
-#include<bits/stdc++.h>
-#include<sys/mman.h>
-#include<stdlib.h>
-#include<sys/wait.h>
-#include<climits>
-#include<time.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <unistd.h>
+#include <time.h>
+#include <iostream>
+#include <pthread.h>
+#include <bits/stdc++.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <climits>
+#include <time.h>
 
 using namespace std;
 
-
-void selection_sort(float v[],int low,int high)
+void selection_sort(float v[], int low, int high)
 {
-    int i=high-low+1;
+    int i = high - low + 1;
 
-    int j=low;
-    float min=v[low];
-    int min_index=low;
+    int j = low;
+    float min = v[low];
+    int min_index = low;
     i--;
-    while(i--)
+    while (i--)
     {
-      
-        for(int k=j+1;k<=high;k++)
-        {
-            if(v[k]<min)
-            {
-                min=v[k];
-                min_index=k;
-            }
 
+        for (int k = j + 1; k <= high; k++)
+        {
+            if (v[k] < min)
+            {
+                min = v[k];
+                min_index = k;
+            }
         }
-        v[min_index]=v[j];
-        v[j]=min;
-       
+        v[min_index] = v[j];
+        v[j] = min;
+
         j++;
-        min=v[j];
-        min_index=j;
+        min = v[j];
+        min_index = j;
     }
     return;
 }
 
-
-void merge(float v[],int low,int mid,int high)
+void merge(float v[], int low, int mid, int high)
 {
-    float v1[mid-low+2],v2[high-mid+1];
+    float v1[mid - low + 2], v2[high - mid + 1];
 
     int i;
-    int zz=0;
-    for( i=low;i<=mid;i++)
+    int zz = 0;
+    for (i = low; i <= mid; i++)
     {
-        v1[zz]=v[i];
+        v1[zz] = v[i];
         zz++;
     }
 
-    v1[zz]=FLT_MAX;
+    v1[zz] = FLT_MAX;
 
     int j;
-    int k=0;
-    for(j=mid+1;j<=high;j++)
+    int k = 0;
+    for (j = mid + 1; j <= high; j++)
     {
-        v2[k]=v[j];
+        v2[k] = v[j];
         k++;
     }
 
-    v2[k]=FLT_MAX;
+    v2[k] = FLT_MAX;
 
-    i=0,j=0;
+    i = 0, j = 0;
 
-    for(int k=low;k<=high;k++)
+    for (int k = low; k <= high; k++)
     {
 
-        if(v1[i] >= v2[j])
-            {
-                v[k]=v2[j];
-                j++;
-            }
+        if (v1[i] >= v2[j])
+        {
+            v[k] = v2[j];
+            j++;
+        }
         else
         {
-            v[k]=v1[i];
+            v[k] = v1[i];
             i++;
         }
     }
-    return ;
+    return;
 }
 
-
-void mergeSort(float v[],int low,int high)
+void mergeSort(float v[], int low, int high)
 {
 
-
-    
-    if(low>=high)
+    if (low >= high)
         return;
 
-    
-    if(high-low+1 <5)
+    if (high - low + 1 < 5)
     {
-        selection_sort(v,low,high);
+        selection_sort(v, low, high);
         return;
     }
 
-    pid_t left_child,right_child,wpid;
-   
-    int status,mid;
+    pid_t left_child, right_child, wpid;
 
-    mid=(low+high)/2;
- 
+    int status, mid;
 
-    left_child=fork();
+    mid = (low + high) / 2;
 
- 
-    if(left_child == 0)
+    left_child = fork();
+
+    if (left_child == 0)
     {
-        mergeSort(v,low,mid);
+        mergeSort(v, low, mid);
 
         _exit(0);
     }
     else
     {
-        right_child=fork();
+        right_child = fork();
 
-        if(right_child == 0){
-        mergeSort(v,mid+1,high);
-        _exit(0);
+        if (right_child == 0)
+        {
+            mergeSort(v, mid + 1, high);
+            _exit(0);
         }
     }
 
-
-    while((wpid = wait(&status))>0);
-    merge(v,low,mid,high);
+    while ((wpid = wait(&status)) > 0)
+        ;
+    merge(v, low, mid, high);
     return;
 }
 
-
-void input_values(float v[],int len)
+void input_values(float v[], int len)
 {
-    for(int i=0;i<len;i++)
+    for (int i = 0; i < len; i++)
     {
-        cin>>v[i];
+        cin >> v[i];
     }
     return;
 }
-
-
 
 int main()
 {
     size_t size;
     int n;
-    cin>>n;
-    
-    clock_t start,end;
+    cin >> n;
 
     float *v;
 
@@ -163,36 +150,38 @@ int main()
 
     // int visibility=MAP_SHARED | MAP_ANONYMOUS;
 
-    size=n*sizeof(float);
-    //v=(int*)mmap(NULL,size,protection,visibility,-1,0);
+    size = n * sizeof(float);
+    // v=(int*)mmap(NULL,size,protection,visibility,-1,0);
 
     int id;
 
-
     key_t key;
 
+    key = ftok("q3.cpp", 1);
 
-    key=ftok("q3.cpp",1);
-    
-    id=shmget(key,size,0666 | IPC_CREAT);
+    id = shmget(key, size, 0666 | IPC_CREAT);
 
-    if(id == -1)
+    if (id == -1)
     {
-        printf("%s",strerror(errno));
-        cout<<"Error";
+        printf("%s", strerror(errno));
+        cout << "Error";
         return 0;
     }
-    
-    v=(float*)shmat(id,NULL,0);
 
-    input_values(v,n);
+    v = (float *)shmat(id, NULL, 0);
 
-   
-    cout<<endl;
-    start=clock();
+    input_values(v, n);
 
-    mergeSort(v,0,n-1);
+    cout << endl;
 
+    auto start = chrono::steady_clock::now();
+
+    mergeSort(v, 0, n - 1);
+
+    auto end = chrono::steady_clock::now();
+    auto diff = end - start;
+    cout << endl
+         << chrono::duration<double, milli>(diff).count() << " ms" << endl;
 
     // for(int i=0;i<n;i++)
     // {
@@ -201,23 +190,22 @@ int main()
 
     // cout<<endl;
 
-    for(int i=0;i<n-1;i++)
+    for (int i = 0; i < n - 1; i++)
     {
-        if(v[i] > v[i+1])
-            cout<<"false";
+        if (v[i] > v[i + 1])
+            cout << "false";
     }
 
-    cout<<endl;
-    int a=shmdt(v);
+    // cout << endl;
+    int a = shmdt(v);
 
-    int detach=shmctl(id,IPC_RMID,NULL);
+    int detach = shmctl(id, IPC_RMID, NULL);
 
-    end=clock();
+    // end = clock();
 
-    double cpu_time_used=((double)(end-start))/CLOCKS_PER_SEC;
+    // double cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
 
-    cout<<cpu_time_used<<endl;
+    // cout << cpu_time_used << endl;
 
-   
     return 0;
 }
